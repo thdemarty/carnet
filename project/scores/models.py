e8bc41865843118f59810408ae5d3cdbcded8a0f
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-04-05 19:34:53 +0200

    feat(scores) add structure button

--- project/scores/models.py
+++ project/scores/models.py
@@ -6,6 +6,7 @@ import uuid
 class Piece(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     name = models.CharField(max_length=100)
+    comment = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
